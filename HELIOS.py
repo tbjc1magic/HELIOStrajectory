061cf6913bc9d3dd6a64c4875761bc5e29a83da7
tbjc <tbjc1magic@aliyun.com>
2015-04-08 17:11:30 -0500

    first GUI

--- HELIOS.py
+++ HELIOS.py
@@ -1,9 +1,36 @@
 import math
 
+__constant_u = 1.66e-27
+__constant_MeV = 1.6e-13
+__constant_e = 1.6e-19
+__constant_c = 3.0e8
 
 
-def trajectory(V, thetalab, charge, mass, B):
-    Vz = V*math.cos(thetalab)
-    Vr = V*math.sin(thetalab)
 
-    OrbitTime 
+
+def trajectory(v, thetalab, charge, mass, B, Resolution =100):
+
+    Pcharge = charge *__constant_e
+    Pmass_tmp = mass* __constant_u
+    Pmass = Pmass_tmp/math.sqrt(1-v*v/(__constant_c*__constant_c))
+
+
+    Vpp = v*math.cos(thetalab)
+    Vpr = v*math.sin(thetalab)
+
+    OrbitRadius = Pmass * Vpr/(Pcharge*B)
+    OrbitTime =  2*math.pi*Pmass/(Pcharge*B)
+   
+    print OrbitRadius, OrbitTime
+    Zpos = []
+    Rpos = []
+
+    for i in xrange(Resolution):
+        TimeStep = OrbitTime/Resolution
+        OrbitTheta = float(i)/Resolution*math.pi*2
+        Rtmp = OrbitRadius*(1-math.cos(OrbitTheta))
+        Ztmp = TimeStep*Vpp*i
+        Rpos.append(Rtmp*100)
+        Zpos.append(Ztmp*100)
+
+    return Zpos, Rpos
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import AtomicMassTable
 import copy
 import math
 import matplotlib.pyplot as plt
-
+import HELIOS
 
 def _quit():
     global root
@@ -38,22 +38,55 @@ Kr.Eex3=0
 lab2 =[]
 K2 = []
 
-for i in xrange(5000):
-    Kr.randomgenerate()
-    lab2.append(Kr.thetalab2)
-    K2.append(Kr.K2)
+
+
+
+
+def ReturnLines(theta=30):
+ 
+    degree = math.pi/180
+    Kr.calculate(theta*degree, 0)
+    MagneticFieldB=2
+    tr2 =  HELIOS.trajectory(Kr.V2, Kr.thetalab2*degree, 10, Kr.m[2], MagneticFieldB)
+    tr3 =  HELIOS.trajectory(Kr.V3, Kr.thetalab3*degree,  1, Kr.m[3], MagneticFieldB)
+   
+    return tr2,tr3
+
+
+tr2, tr3 = ReturnLines(30)
+
+#for i in xrange(5000):
+#    Kr.randomgenerate()
+#    lab2.append(Kr.thetalab2)
+#    K2.append(Kr.K2)
 
 fig = plt.figure(1)
 ax = fig.add_subplot(111)
 ax.grid(True)
-
-line  = ax.plot(lab2,K2,'.')
+ax.set_xlim([-150,150])
+ax.set_ylim([-10,50])
+line2 = ax.plot(tr2[0],tr2[1])
+line3 = ax.plot(tr3[0],tr3[1],c='r')
+ax.axhline(0, color='black')
+ax.axvline(0, color='black')
 
 root  = Tkinter.Tk()
+root.minsize(width=1000,height=600)
 root.protocol("WM_DELETE_WINDOW",_quit)
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas.show()
-canvas.get_tk_widget().place(relwidth=0.8, relheight=0.8,relx=0.1,rely=0.1)
+canvas.get_tk_widget().place(relwidth=0.6, relheight=0.9,relx=0.05,rely=0.05)
+
+def s2changed(*args):
+    thetalab2 = float(args[0])
+    tr2,tr3 = ReturnLines(thetalab2)
+    line2[0].set_data(tr2[0],tr2[1])
+    line3[0].set_data(tr3[0],tr3[1])
+    canvas.draw() 
+
+s2 = Tkinter.Scale(root, from_=0.1, to=179, resolution = 0.1,command=s2changed)
+s2.set(30)
+s2.place(relx = 0.7,rely = 0.1)
 
 #print Kr.m
 #Kr.calculate(math.pi/2,1.2)
